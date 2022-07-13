bc205f8178ac8f952295392e82c7f9bac55caae7
Shelton <18927858896@163.com>
2022-07-13 15:38:05 +0800

    E0045. Handles 自定义 3D GUI 空间 和 绘制

---
+++ Assets/EditorExtension/07.Handles/Editor/HandlesExampleEditor.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace EditorExtension.Handles.Editor
+{
+    [CustomEditor(typeof(HandlesExample))]
+    public class HandlesExampleEditor : UnityEditor.Editor
+    {
+        private void OnSceneGUI()
+        {
+            var t = target as HandlesExample;
+            UnityEditor.Handles.color = new Color(1, 0.8f, 0.4f, 1);
+            UnityEditor.Handles.DrawWireDisc(t.transform.position, t.transform.up, t.Radius);
+            UnityEditor.Handles.Label(t.transform.position,t.Radius.ToString("F1"));
+            
+            UnityEditor.Handles.BeginGUI();
+            {
+                if (GUILayout.Button("Hello"))
+                {
+                    Debug.Log("Hello");
+                }
+            }
+            UnityEditor.Handles.EndGUI();
+        }
+    }
+}
---
+++ Assets/EditorExtension/07.Handles/HandlesExample.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace EditorExtension.Handles
+{
+    public class HandlesExample : MonoBehaviour
+    {
+        /// <summary>
+        /// °ë¾¶
+        /// </summary>
+        public float Radius = 0.7f;
+    }
+}
